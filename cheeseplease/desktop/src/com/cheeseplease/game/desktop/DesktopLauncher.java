7592b994bd4c7e9dbcd138fb981ba62d02fa2967
Paulo Kinjo <paulokinjo@hotmail.com>
2022-01-03 15:30:31 +0900

    Cheeseplease v1

---
+++ cheeseplease/desktop/src/com/cheeseplease/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cheeseplease.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cheeseplease.game.ChessePleaseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChessePleaseGame(), config);
+	}
+}
