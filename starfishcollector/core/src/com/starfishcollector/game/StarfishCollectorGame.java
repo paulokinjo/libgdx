7410b33f094e545adc5e4635338906c67c676827
Paulo Kinjo <paulokinjo@hotmail.com>
2022-01-03 14:30:54 +0900

    first commit

---
+++ starfishcollector/core/src/com/starfishcollector/game/StarfishCollectorGame.java
@@ -0,0 +1,44 @@
+package com.starfishcollector.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class StarfishCollectorGame extends ApplicationAdapter {
+	private Stage _mainStage;
+	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	public void initialize() {}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+
+	public void load(Stage mainStage) {
+		this._mainStage = mainStage;
+		this.initialize();
+	}
+
+	public Stage getMainStage() {
+		return this._mainStage;
+	}
+}
---
+++ starfishcollector/desktop/src/com/starfishcollector/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.starfishcollector.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.starfishcollector.game.StarfishCollectorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarfishCollectorGame(), config);
+	}
+}
