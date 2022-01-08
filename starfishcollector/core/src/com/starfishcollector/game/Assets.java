da480e5101d980f310beecb2ebfe9a77b50b1b17
Paulo Kinjo <paulokinjo@hotmail.com>
2022-01-08 18:09:21 +0900

    (starfishcollector): Add UnitTest fully with first version

---
+++ starfishcollector/core/src/com/starfishcollector/game/Assets.java
@@ -0,0 +1,8 @@
+package com.starfishcollector.game;
+
+public abstract class Assets {
+    public static final String TURTLE_1_PNG = "turtle-1.png";
+    public static final String STARFISH_PNG = "starfish.png";
+    public static final String OCEAN_JPG = "ocean.jpg";
+    public static final String WIN_PNG = "win.png";
+}
--- starfishcollector/core/src/com/starfishcollector/game/StarfishCollectorGame.java
+++ starfishcollector/core/src/com/starfishcollector/game/StarfishCollectorGame.java
@@ -1,44 +1,131 @@
 package com.starfishcollector.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.Rectangle;
 
 public class StarfishCollectorGame extends ApplicationAdapter {
-	private Stage _mainStage;
-	SpriteBatch batch;
-	Texture img;
+    private SpriteBatch spriteBatch;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    private Texture turtleTexture;
+    private Rectangle turtleRectangle;
+    private float turtleX;
+    private float turtleY;
 
-	public void initialize() {}
+    private Texture oceanTexture;
+    private float oceanX;
+    private float oceanY;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private Texture starfishTexture;
+    private float starfishX;
+    private float starfishY;
+    private Rectangle starfishRectangle;
 
-	public void load(Stage mainStage) {
-		this._mainStage = mainStage;
-		this.initialize();
-	}
+    private Texture winTexture;
+    private boolean win;
+    private float winX;
+    private float winY;
 
-	public Stage getMainStage() {
-		return this._mainStage;
-	}
+    @Override
+    public void create() {
+        this.initialize(new SpriteBatch());
+    }
+
+    @Override
+    public void render() {
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            turtleX--;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            turtleX++;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            turtleY++;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            turtleY--;
+        }
+
+        this.turtleRectangle.setPosition(turtleX, turtleY);
+
+        if(turtleRectangle.overlaps(starfishRectangle)) {
+            win = true;
+        }
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        spriteBatch.begin();
+        spriteBatch.draw(oceanTexture, oceanX, oceanY);
+
+        if(!win) {
+            spriteBatch.draw(starfishTexture, starfishX, starfishY);
+            spriteBatch.draw(turtleTexture, turtleX, turtleY);
+        } else {
+            spriteBatch.draw(winTexture, winX, winY);
+        }
+
+        spriteBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+    }
+
+    public void initialize(SpriteBatch spriteBatch) {
+        this.spriteBatch = spriteBatch;
+
+        turtleTexture = new Texture(Gdx.files.internal(Assets.TURTLE_1_PNG));
+        turtleX = 20;
+        turtleY = 20;
+        turtleRectangle = new Rectangle(turtleX, turtleY, turtleTexture.getWidth(), turtleTexture.getHeight());
+
+        oceanTexture = new Texture(Gdx.files.internal(Assets.OCEAN_JPG));
+        oceanX = 0;
+        oceanY = 0;
+
+        starfishTexture = new Texture(Gdx.files.internal(Assets.STARFISH_PNG));
+        starfishX = 380;
+        starfishY = 380;
+        starfishRectangle = new Rectangle(starfishX, starfishY, starfishTexture.getWidth(), starfishTexture.getHeight());
+
+        winTexture = new Texture(Gdx.files.internal(Assets.WIN_PNG));
+        winX = 100;
+        winY = 180;
+
+        win = false;
+    }
+
+    public Texture getTurtleTexture() {
+        return turtleTexture;
+    }
+
+    public Rectangle getTurtleRectangle() {
+        return turtleRectangle;
+    }
+
+    public Texture getOceanTexture() {
+        return oceanTexture;
+    }
+
+    public Texture getStarfishTexture() {
+        return starfishTexture;
+    }
+
+    public Rectangle getStarfishRectangle() {
+        return starfishRectangle;
+    }
+
+    public Texture getWinTexture() {
+        return winTexture;
+    }
 }
